7f18cbfe3e5b5cdffc2e1fc0586cf805bce562d1
vikrantsrgc <145971065+vikrantsrgc@users.noreply.github.com>
2025-04-05 12:36:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
-//complete this code
+// 1. Rectangle Class
 class Rectangle {
-	constructor(width,height){
-		this._width = width
-		this._height = height 
-		
-	}
-	get width(){
-		return this._width
-	}
-	get height(){
-		return this._height
-	}
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-	getArea(){
-		return this._width * this._height
-	}
+  // Getter for width
+  get width() {
+    return this._width;
+  }
+
+  // Getter for height
+  get height() {
+    return this._height;
+  }
+
+  // Method to calculate area
+  getArea() {
+    return this._width * this._height;
+  }
 }
 
+// 2. Square Class (inherits from Rectangle)
 class Square extends Rectangle {
-	constructor(side){
-   super(side , side)
-	
-	}
-	getPerimeter(){
-		return 4 * this.width
-	}
+  constructor(side) {
+    super(side, side); // Set width and height to side
+  }
+
+  // Method to calculate perimeter
+  getPerimeter() {
+    return this.width * 4;
+  }
 }
+
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
